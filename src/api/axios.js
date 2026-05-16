8906a23b1383e3c45a777535524cd6f31ded7ad0
Sai Kumar <saikumar13637@gmail.com>
2026-05-16 17:40:33 +0530

    updated axios api url and jwt integration

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,21 +2,22 @@ import axios from "axios";
 
 const API = axios.create({
 
-    baseURL: "http://localhost:8080/api"
+    baseURL: "https://saikumar-edutech-backend-1.onrender.com/api"
+
 });
 
 API.interceptors.request.use((config) => {
 
-    const token =
-        localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
-    if (token) {
+    if(token){
 
         config.headers.Authorization =
             `Bearer ${token}`;
     }
 
     return config;
+
 });
 
 export default API;
