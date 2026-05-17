1b891fd949ddc9fbd30197dc1f76e65347d6ab23
Sai Kumar <saikumar13637@gmail.com>
2026-05-17 14:18:00 +0530

    chat

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,56 +1,25 @@
-<div className="chat-page">
+function Chat() {
 
-    <div className="chat-users">
+    return (
 
-        <h2>Chats</h2>
+        <div className="chat-page">
 
-        {users.map(user=>(
-            <div className="user-item">
+            <div className="chat-users">
 
-                <div className="user-name">
-                    {user.username}
+                <h2>Chats</h2>
+
+            </div>
+
+            <div className="chat-box">
+
+                <div className="chat-header">
+                    Select User To Start Chat
                 </div>
 
-                <div className="user-email">
-                    {user.email}
-                </div>
-
-            </div>
-        ))}
-
-    </div>
-
-    <div className="chat-box">
-
-        <div className="chat-header">
-            Select User To Start Chat
-        </div>
-
-        <div className="chat-messages">
-
-            <div className="message-left">
-                Hello
-            </div>
-
-            <div className="message-right">
-                Hi
             </div>
 
         </div>
+    );
+}
 
-        <div className="chat-input-box">
-
-            <input
-                className="chat-input"
-                placeholder="Type message..."
-            />
-
-            <button className="send-btn">
-                Send
-            </button>
-
-        </div>
-
-    </div>
-
-</div>
+export default Chat;
