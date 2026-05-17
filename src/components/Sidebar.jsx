7c088451d5074b17bd9713e38e4ffb734a88ef29
Sai Kumar <saikumar13637@gmail.com>
2026-05-17 14:11:00 +0530

    side navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+import {
+    FaHome,
+    FaImage,
+    FaUpload,
+    FaComments
+} from "react-icons/fa";
+
 function Sidebar({ open, setOpen }) {
 
     return (
@@ -15,10 +24,43 @@ function Sidebar({ open, setOpen }) {
                 SAI
             </div>
 
-            {/* sidebar menu */}
+            <div className="sidebar-menu">
+
+                <NavLink
+                    to="/dashboard"
+                    onClick={()=>setOpen(false)}
+                >
+                    <FaHome/>
+                    <span>Dashboard</span>
+                </NavLink>
+
+                <NavLink
+                    to="/feed"
+                    onClick={()=>setOpen(false)}
+                >
+                    <FaImage/>
+                    <span>Feed</span>
+                </NavLink>
+
+                <NavLink
+                    to="/upload"
+                    onClick={()=>setOpen(false)}
+                >
+                    <FaUpload/>
+                    <span>Upload</span>
+                </NavLink>
+
+                <NavLink
+                    to="/chat"
+                    onClick={()=>setOpen(false)}
+                >
+                    <FaComments/>
+                    <span>Chat</span>
+                </NavLink>
+
+            </div>
 
         </div>
-
     );
 }
 
